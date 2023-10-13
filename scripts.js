2f53f01da1c669c12cdcc361bdb7eff5be609ea7
Bruno dos Santos Guterres <brunoguterres@cobrape.com.br>
2023-10-13 16:19:50 -0300

    adicionando opção de fundo com imagem google

--- scripts.js
+++ scripts.js
@@ -8,11 +8,22 @@ function onEachFeature(feature, layer) {
 //var map = L.map('map').setView([-25, -51], 7);    //set view bacia tietê e iguaçu
 var map = L.map('map').setView([-25.425, -49.220], 16);
 
+/*
+//Carragamento de mapa OSM de fundo
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 osm.addTo(map);
+*/
+
+//Carregamento de imagem Google Maps de fundo
+var fundo_satelite = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+    maxZoom: 22,
+    attribution: 'Imagem de satélite © <a href="https://www.google.com/maps">Google Maps</a>'
+}).addTo(map);
+
+fundo_satelite.addTo(map);
 
 var states = [{
     "type": "Feature",
