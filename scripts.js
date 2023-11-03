e3e15026073d8c1264e0c9f72423f8557f397c81
brunoguterres <brunoguterres26@gmail.com>
2023-11-03 18:05:19 -0300

    seleção ottobacia e montante a partir do clique

--- scripts.js
+++ scripts.js
@@ -1,13 +1,33 @@
 function onEachFeature(feature, layer) {
     layer.on('click', function (e) {
         var cobaciaValue = feature.properties.cobacia;
-        
-        var popupContent = "Cobacia: " + cobaciaValue;
+
+        var cobaciaValueEdit = cobaciaValue;
+        while (cobaciaValueEdit.length > 0 && parseInt(cobaciaValueEdit.slice(-1)) % 2 !== 0) {
+            cobaciaValueEdit = cobaciaValueEdit.slice(0, -1); // Remove o último dígito
+        };
+    
+        var popupContent = "Cobacia Original: " + cobaciaValue + "<br>Cobacia Editada: " + cobaciaValueEdit;
         
         L.popup()
             .setLatLng(e.latlng)
             .setContent(popupContent)
             .openOn(map);
+        
+        var ottobaciasMontante = L.Geoserver.wfs('http://localhost:8080/geoserver/wfs', {
+            layers: 'teste:__view_teste_iguacu',
+            className: 'camada_ottobacias_montante',
+            CQL_FILTER: "cobacia LIKE '"+cobaciaValueEdit+"%' AND cobacia > '"+cobaciaValue+"'"
+        });
+        ottobaciasMontante.addTo(map);
+        
+        var ottobaciaSelecionada = L.Geoserver.wfs('http://localhost:8080/geoserver/wfs', {
+            layers: 'teste:__view_teste_iguacu',
+            className: 'camada_ottobacias_selecionada',
+            CQL_FILTER: "cobacia LIKE '"+cobaciaValue+"'"
+        });
+        ottobaciaSelecionada.addTo(map);
+        
     });
 }
 
@@ -35,12 +55,6 @@ var ottobacias = L.Geoserver.wfs('http://localhost:8080/geoserver/wfs', {
     onEachFeature: onEachFeature
 });
 
-var ottobacias_montante = L.Geoserver.wfs('http://localhost:8080/geoserver/wfs', {
-    layers: 'teste:__view_teste_iguacu',
-    className: 'camada_ottobacias_montante',
-    CQL_FILTER: "cobacia LIKE '86286%'"
-});
-
 var map = L.map('map', {
     center: [-15, -51.5],
     zoom: 4,
@@ -54,8 +68,9 @@ var baseMaps = {
 };
 
 var overlayMaps = {
-    "Ottobacias": ottobacias,
-    "Ottobacias à Montante": ottobacias_montante
+    "Ottobacias": ottobacias
 };
 
-var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+var layerControl = L.control.layers(baseMaps, overlayMaps);
+
+layerControl.addTo(map);
